32f1b010c1c0970dd4995e4090edd5f1a00de7aa
Thoq-jar <tristanshaw000@gmail.com>
2024-08-11 14:50:32 -0400

    updated stuff

--- .pcss/pcss.js
+++ .pcss/pcss.js
@@ -1,1 +1,3 @@
+/* sof */
 async function injectBCSS(t){try{const e=await fetch(t);e.ok||console.error("BCSS Failed to load!");const c=await e.text(),n=document.createElement("style");n.innerText=c,n.innerText=c,document.head.appendChild(n)}catch({}){console.error("BCSS Failed to load!")}}const cssURL="https://raw.githubusercontent.com/Thoq-jar/PCSS/master/.pcss/pcss.css?token=GHSAT0AAAAAACV7HKLY4NBUXGHPA22CHPUGZVZATYA";injectBCSS(cssURL).then((t=>{}));
+/* eof */
